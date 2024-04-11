d02ea80c51a1519f86fd36de2b08fd7759baa961
patricj <pehunter@wpi.edu>
2024-04-11 12:59:44 -0400

    cube

--- main.js
+++ main.js
@@ -8,3 +8,44 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+function createLights() {
+    // Create one point light and add it to the scene
+    const pointLight = new THREE.DirectionalLight( 0xcccccc, 10 )  
+
+    // Set the point light's position
+    pointLight.position.z = 100
+
+    // Add the light to the scene
+    scene.add( pointLight )
+  }
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x007F00, shininess: 2000 } );
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(1,4,100)
+scene.add( cube );
+
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 1, 4, 100 );
+createLights();
+// camera.position.z = 5;
+
+// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+// const line = new THREE.Line( geometry, material );
+// scene.add( line );
+// renderer.render( scene, camera );
+
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
