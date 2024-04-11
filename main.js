a677069b4f39e5d53b07a45217f703ead11e3519
patricj <pehunter@wpi.edu>
2024-04-11 13:36:52 -0400

    added camera??

--- main.js
+++ main.js
@@ -4,48 +4,92 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const animateFuncs = []
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-function createLights() {
+function createLights(x,y,z) {
     // Create one point light and add it to the scene
-    const pointLight = new THREE.PointLight( 0xcccccc, 10 )  
+    const pointLight = new THREE.PointLight( 0xcccccc, 1000 )  
 
     // Set the point light's position
-    pointLight.position.z = 100
+    pointLight.position.x = x;
+    pointLight.position.y = y;
+    pointLight.position.z = z;
 
     // Add the light to the scene
     scene.add( pointLight )
   }
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x007F00, shininess: 2000 } );
-const cube = new THREE.Mesh( geometry, material );
-cube.position.set(1,4,100)
-scene.add( cube );
+function createGeometry() {
+    for(let i = 0; i < 15; i++) {
+        const geometry = new THREE.BoxGeometry( i, i, i );
+        const material = new THREE.MeshPhongMaterial( { color: Math.floor(Math.random()*16777215), shininess: 2000 } );
+        const cube = new THREE.Mesh( geometry, material );
+        cube.position.set(Math.floor(Math.random()*100) - 50,0,Math.floor(Math.random()*100) - 50)
+        animateFuncs.push(() => rotateObj(cube))
+        scene.add( cube );
+    }
+}
 
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 1, 4, 100 );
-createLights();
-// camera.position.z = 5;
+document.addEventListener("keydown", (e) => {
+    var key_press = e.key;
+    var key_code = key_press.charCodeAt(0);
+    console.log(key_code);
+    let dir = camera.rotation;
+    let angle = 0;
+    let speed = 0.5;
+    switch(key_code) {
+        case 115:
+            speed = speed;
+            break;
+        case 119:
+            speed = -speed;
+            break;
+        case 97:
+            speed = 0;
+            angle = 1;
+            break;
+        case 100:
+            speed = 0;
+            angle = -1;
+            break;
+        default:
+            speed = 0;
+    }
+    let vec = new THREE.Vector3(0,0,1);
+    vec.applyEuler(dir);
+    vec.multiplyScalar(speed);
+    // console.log(vec);
+    adjustCamera(vec, angle);
+})
 
-// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-// const points = [];
-// points.push( new THREE.Vector3( - 10, 0, 0 ) );
-// points.push( new THREE.Vector3( 0, 10, 0 ) );
-// points.push( new THREE.Vector3( 10, 0, 0 ) );
+function adjustCamera(pos_change, angle) {
+    console.log(camera.position)
+    // pos_change.add(camera.position)
+    camera.position.x += pos_change.x;
+    camera.position.y += pos_change.y;
+    camera.position.z += pos_change.z;
 
-// const geometry = new THREE.BufferGeometry().setFromPoints( points );
-// const line = new THREE.Line( geometry, material );
-// scene.add( line );
-// renderer.render( scene, camera );
+    camera.rotateOnAxis(new THREE.Vector3(0,1,0), (Math.PI/180)*angle);
+    // camera.lookAt( 1, 4, 100 );
+}
+
+createGeometry();
+createLights(3,3,0);
+camera.position.set( 0, 0, 0 );
+
+function rotateObj(cube) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    animateFuncs.forEach((f) => {
+        f()
+    })
 	renderer.render( scene, camera );
 }
 animate();
